9c7275e592d496812d53268cef1688ea0e66542f
BeritJanssen <berit.janssen@gmail.com>
2025-07-14 17:03:59 +0200

    fix: correct logic for invalid keys

--- code/generate_models.py
+++ code/generate_models.py
@@ -109,7 +109,7 @@ def generate_models(
         'vector_size',
         'window_size',
     ]
-    invalid_keys = set(corpus_config.keys()).issubset(set(valid_keys))
+    invalid_keys = set(corpus_config.keys()) - set(valid_keys)
     if invalid_keys:
         raise CorpusConfigurationException(
             f"The following keys are invalid: {', '.join(invalid_keys)}"
