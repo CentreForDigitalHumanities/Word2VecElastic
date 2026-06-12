e8612d83d8d827f3c42724e2857e4fa5c838600c
Luka van der Plas <lukavdplas@mailfence.com>
2026-06-12 13:36:12 +0200

    more debug message

--- code/collect_sentences.py
+++ code/collect_sentences.py
@@ -172,6 +172,7 @@ class DataCollector():
             scroll_id = docs['_scroll_id']
             try:
                 docs = es.scroll(scroll_id=scroll_id, scroll="60m")
+                logger.info(docs)
             except Exception as e:
                 logger.warning(e)
                 time.sleep(10)
